c567a02954647019b321831f2a8324a49328903a
biu <hezhou920@gmail.com>
2018-01-12 17:15:44 +0800

    登出

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  StyleSheet
+} from 'react-native';
+
+import Button from '../components/Button';
+import navigationUtil from '../utils/navigation';
 
 class Profile extends Component {
   render() {
     return (
-      <View>
-        <Text>Profile</Text>
+      <View style={styles.container}>
+        <Button
+          onPress={this.logout}
+        >
+          退出登录
+        </Button>
       </View>
     );
   }
+
+  logout = () => {
+    navigationUtil.reset(this.props.navigation, 'Login');
+  };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16
+  }
+});
 
 export default Profile;
